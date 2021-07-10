2e12d777b6a379b847d79e6258b74c0658b96b36
Mervin John Tampus <mervinjohn.tampus@cit.edu>
2021-07-10 14:35:35 +0800

    update 2nd screen

--- lib/screens/2ndscreen.dart
+++ lib/screens/2ndscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recycler/screens/home.dart';
 
 class Secondscreen extends StatefulWidget {
   @override
@@ -11,56 +12,90 @@ class _SecondscreenState extends State<Secondscreen> {
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-          height: height,
-          width: width,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("images/2ndpoint.png"),
-                fit: BoxFit.fitWidth,
-                alignment: Alignment.center),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 300),
-              Container(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      body: SafeArea(
+        child: Container(
+            height: height,
+            width: width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/2ndpoint.png"),
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.center),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Spacer(),
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    child: Text(
+                      'Hi!',
+                      style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Cooper Black',
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
                   child: Text(
-                    'Hi!',
+                    'My name is',
                     style: TextStyle(
-                      fontSize: 45,
+                      fontSize: 40,
                       fontWeight: FontWeight.bold,
                       fontFamily: 'Cooper Black',
                       color: Colors.black,
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
-                child: Text(
-                  'My name is',
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Cooper Black',
-                    color: Colors.black,
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Enter your name',
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    border: UnderlineInputBorder(),
-                    labelText: 'Enter your name',
+                SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  height: 50,
+                  width: 200,
+                  decoration: new BoxDecoration(
+                      color: const Color(0xFF9378FF),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.purple.shade200,
+                          spreadRadius: 3,
+                          blurRadius: 10,
+                          offset: Offset(0, 2), // changes position of shadow
+                        ),
+                      ],
+                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(
+                          fontSize: 16, fontFamily: 'Public Sans'),
+                    ),
+                    child: const Text(
+                      'Back',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () => {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Home()))
+                    },
                   ),
                 ),
-              ),
-            ],
-          )),
+              ],
+            )),
+      ),
     );
   }
 }
