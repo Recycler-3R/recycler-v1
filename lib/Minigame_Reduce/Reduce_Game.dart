e51b9fd57e8e9f5dfa8d0cf3f4af71f73f6d45c0
Jezreel Floreta <cyberblazeflo7@gmail.com>
2021-07-17 03:55:11 +0800

    Provided mechanism to end the game (partially working)

--- lib/Minigame_Reduce/Reduce_Game.dart
+++ lib/Minigame_Reduce/Reduce_Game.dart
@@ -1,13 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:recycler/Minigame_Reduce/Tiles.dart';
 
-int rand() {
-  var rand = new Random();
-  return rand.nextInt(3);
-}
-
-// Color colorPicker() {}
+// int rand() {
+//   var rand = new Random();
+//   return rand.nextInt(3);
+// }
 
 class Reduce extends StatefulWidget {
   const Reduce({Key? key}) : super(key: key);
@@ -18,13 +18,14 @@ class Reduce extends StatefulWidget {
 
 class _ReduceState extends State<Reduce> {
   var rand = new Random();
+  var ongoingGame = true;
+  int chosenColorActive = 0;
 
   List<Color> colorPool = [
     Colors.deepPurple,
     Colors.green,
     Colors.pink.shade100,
   ];
-  // int currentColor = rand();
 
   Map<int, Color> colorGrid = Map();
 
@@ -43,38 +44,60 @@ class _ReduceState extends State<Reduce> {
   //   11: Colors.green,
   // };
 
-  // var colorGrid = {0: Colors.blue.shade100};
-
-  // var get colorValue() {
-  //   return colorGrid
-  // }
-
   setColorGrid(int index) {
     colorGrid.putIfAbsent(index, () => colorPool[rand.nextInt(3)]);
     colorGrid.update(index, (value) => colorPool[rand.nextInt(3)]);
-    //return val;
-    //colorGrid[index] = colorPool[rand.nextInt(3)];
   }
 
   getColorGrid(int index) {
     if (colorGrid.containsKey(index)) {
       return colorGrid[index];
     }
-    //return colorGrid[index].toList();
   }
 
-  // initColorGrid() {
-  //   for (int i = 0; i < colorGrid.length; i++) {
-  //     setColorGrid(i);
-  //     //print('Color of Tile[' + i.toString() + ']: ' + getColorGrid(i));
-  //   }
-  // }
+  void startGame(Map board, Color chosen) {
+    Timer.periodic(
+      Duration(milliseconds: 10),
+      (timer) {
+        chosenColorActive = 0;
+        checkBoard(board, chosen);
+
+        // setState(() {
+
+        // });
+      },
+    );
+  }
+
+  void spawner(int index) {
+    Timer.periodic(Duration(milliseconds: 1200), (timer) {
+      //add loading/growing animation
+      setState(() {
+        setColorGrid(index);
+      });
+    });
+  }
+
+  void checkBoard(Map board, Color chosen) {
+    for (Color currentColor in board.values) {
+      if (currentColor == chosen) {
+        chosenColorActive++;
+      }
+    }
+    if (chosenColorActive == 0) {
+      ongoingGame = false;
+    } else {
+      ongoingGame = true;
+    }
+  }
 
   @override
   void initState() {
     for (int i = 0; i < 12; i++) {
       setColorGrid(i);
       //print('Color of Tile[' + i.toString() + ']: ' + getColorGrid(i));
+      startGame(colorGrid, Colors.deepPurple);
+      // startGame(colorGrid, colorPool[rand.nextInt(3)]);
     }
     super.initState();
   }
@@ -96,7 +119,15 @@ class _ReduceState extends State<Reduce> {
                           color: getColorGrid(index),
                           onTap: () {
                             setState(() {
-                              setColorGrid(index);
+                              if (ongoingGame) {
+                                setColorGrid(index);
+                                checkBoard(colorGrid, Colors.deepPurple);
+                                if (ongoingGame == false) {
+                                  Navigator.pop(context);
+                                }
+                              } else {
+                                Navigator.pop(context);
+                              }
                             });
                           });
                     }),
